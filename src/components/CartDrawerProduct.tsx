7748f366dfdebcee469ebe04126a7210203d9720
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-10-22 20:56:10 +0300

    Added quantity

--- src/components/CartDrawerProduct.tsx
+++ src/components/CartDrawerProduct.tsx
@@ -1,22 +1,37 @@
 import { useAppSelector } from "@/app/store"
-import { Box ,Flex, Image, Text, VStack } from "@chakra-ui/react"
-
+import { Box ,Flex, HStack, IconButton, Image, Text, VStack } from "@chakra-ui/react"
+import { FiTrash2, FiPlus, FiMinus } from "react-icons/fi";
 
 const CartDrawerProduct = () => {
     const {cartItems} = useAppSelector((store) => store.cart)
     console.log(cartItems)
     const totalPrice: number = cartItems.reduce((acc , itme) => acc + itme.price, 0)
-    
+
     return (<>
             {cartItems.length > 0 ? <>
                 {cartItems.map((item) => (
                     <Box key={item.id} p={3} borderBottom="1px solid" borderColor="gray.200">
-                        <Flex justifyContent={"space-evenly"} alignItems={""}>
+                        <Flex justifyContent={"space-between"} px={"5"} alignItems={"center"}>
                             <Image src={`${import.meta.env.VITE_LOCAL_API}${item.thumbnail.url}`} alt={item.title} boxSize="80px" borderRadius="md" />
                             <VStack mt={2}>
                             <Text fontWeight="medium">{item.title}</Text>
                             <Text color="teal.500">${item.price}</Text>
+                            <HStack>
+                                <IconButton size="xs" aria-label="Decrease quantity">
+                                    <FiMinus />
+                                </IconButton>
+                                <Text>1</Text>
+                                <IconButton size="xs" aria-label="Increase quantity">
+                                    <FiPlus />
+                                </IconButton>
+                            </HStack>
                             </VStack>
+                            <IconButton aria-label="Remove item"
+                            variant={"ghost"}
+                            colorScheme={"red"}
+                            color={"red"}>
+                                <FiTrash2 />
+                            </IconButton>
                         </Flex>
                     </Box>
                 ))}
@@ -28,7 +43,6 @@ const CartDrawerProduct = () => {
                         ${totalPrice.toFixed(2)}
                     </Text>
                 </Flex>
-
             </>:
             (
                 <Box textAlign="center" py={8}>
